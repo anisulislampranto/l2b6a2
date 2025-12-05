2d24f22791d55fe10fd67251d482181343a765d3
anisulislampranto <76773444+anisulislampranto@users.noreply.github.com>
2025-12-05 13:13:26 +0600

    vehicles pool query

--- src/config/db.ts
+++ src/config/db.ts
@@ -19,6 +19,17 @@ const initDB = async () => {
         )
     `)
 
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS vehicles (
+            id SERIAL PRIMARY KEY,
+            vehicle_name VARCHAR(100) NOT NULL,
+            type VARCHAR(20) NOT NULL CHECK (type IN ('car', 'bike', 'van', 'SUV')),
+            registration_number VARCHAR(150) UNIQUE NOT NULL,
+            daily_rent_price NUMERIC NOT NULL CHECK (daily_rent_price > 0),
+            availability_status VARCHAR(20) NOT NULL CHECK (availability_status IN ('available', 'booked'))
+        )
+    `)
+
 }
 
 export default initDB;
