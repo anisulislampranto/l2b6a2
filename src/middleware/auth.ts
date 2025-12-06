cfbe6581b67661f2da511a460d4479b42d595dc9
anisulislampranto <76773444+anisulislampranto@users.noreply.github.com>
2025-12-06 22:56:31 +0600

    Bearer token approach

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -5,7 +5,13 @@ import config from '../config';
 const auth = (...roles: string[]) => {
     return async (req: Request, res: Response, next: NextFunction) => {
         try {
-            const token = req.headers.authorization;
+            const authHeader = req.headers.authorization;
+
+            if (!authHeader || !authHeader.startsWith("Bearer ")) {
+                return res.status(401).json({ message: "Please login to perform this action!" });
+            }
+
+            const token = authHeader.split(" ")[1];
 
             if (!token) {
                 return res.status(500).json({ message: 'Please login to perform this action!' })
